12f3936e9b7f8901732994647e0e34b46fca1580
Normandez <sergienko.9711@gmail.com>
2019-09-02 21:56:38 +0300

    Added WinServer CLI control panel

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,7 @@
 #include "CApplication.h"
 
+static const char* s_main_menu = "CONTROL PANEL:\n\t0 - exit\n\t1 - print work threads num\n\t2 - print work threads\n\t3 - stop all work threads\n\n";
+
 int main( int argc, char** argv )
 {
 	try
@@ -8,11 +10,31 @@ int main( int argc, char** argv )
 		app.Listen();
 		
 		short res = 255;
-		std::cout << "Enter '0' to exit..." << std::endl;
+		std::cout << s_main_menu << std::endl;
 		while(std::cin)
 		{
 			std::cin >> res;
-			if(!res) break;
+
+			if( res == 0 )
+			{
+				break;
+			}
+			else if( res == 1 )
+			{
+				std::cout << "Work threads num = " << app.GetWorkThreadNum() << std::endl;
+			}
+			else if( res == 2 )
+			{
+				app.PrintWorkThreads();
+			}
+			else if( res == 3 )
+			{
+				app.StopAllThreads();
+			}
+			else
+			{
+				std::cout << "Invalid choice. Try again." << std::endl;
+			}
 		}
 	}
 	catch( const std::exception& rt_ex )
